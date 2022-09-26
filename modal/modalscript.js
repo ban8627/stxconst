ab98071867647985c3d3ce390917d7435aca8805
banssa <ban8627@gmail.com>
2022-09-26 15:36:51 +0900

    add modal

---
+++ modal/modalscript.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  // modal
+  $("html").css("overflow", "hidden");
+
+  const modalWrap = $(".modal-wrap");
+  const modalClose = $(".modal-close");
+
+  modalClose.click(() => {
+    modalWrap.stop().fadeOut(100);
+    $("html").css("overflow", "hidden");
+  });
+
+  const modalMain = $(".modal-main");
+  modalMain.click((event) => {
+    event.stopPropagation();
+  });
+  modalWrap.click(() => {
+    modalWrap.stop().fadeOut(100);
+    $("html").css("overflow", "hidden");
+  });
+});
